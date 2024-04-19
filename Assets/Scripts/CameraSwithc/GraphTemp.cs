21fd3162856d7de681c49e87c0135f28bdc020b1
JiaqiFlora <jiaqigan.kiki@gmail.com>
2024-04-19 01:11:44 -0400

    switch camera

---
+++ Assets/Scripts/CameraSwithc/GraphTemp.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GraphTemp : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera firstCam;
+    [SerializeField] CinemachineVirtualCamera secondCam;
+
+    private void OnEnable()
+    {
+        CameraSwitcher.Register(firstCam);
+        CameraSwitcher.Register(secondCam);
+
+        CameraSwitcher.SwitchCamera(firstCam);
+    }
+
+    private void OnDisable()
+    {
+        CameraSwitcher.Unregister(firstCam);
+        CameraSwitcher.Unregister(secondCam);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // switch camera
+            if(CameraSwitcher.IsActiveCamera(secondCam))
+            {
+                CameraSwitcher.SwitchCamera(firstCam);
+            }
+            else if(CameraSwitcher.IsActiveCamera(firstCam))
+            {
+                CameraSwitcher.SwitchCamera(secondCam);
+            }
+        }
+    }
+}
