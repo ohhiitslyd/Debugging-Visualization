9bf3191cab86831443c78b14b40f659645c074d4
JiaqiFlora <jiaqigan.kiki@gmail.com>
2024-05-24 03:48:51 -0400

    change color to highlight

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+    public Material highlightMat;
+    public Material material;
 
     public void Highlight()
     {
-        gameObject.AddComponent<Outline>();
-        Outline outline = GetComponent<Outline>();
-        outline.OutlineColor = Color.yellow;
-        outline.OutlineWidth = 3.0f;
+        //gameObject.AddComponent<Outline>();
+        //Outline outline = GetComponent<Outline>();
+        //outline.OutlineColor = Color.yellow;
+        //outline.OutlineWidth = 3.0f;
+
+
+        Renderer[] childrenRenderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in childrenRenderers)
+        {
+            rend.material = highlightMat;
+        }
     }
 
 
     public void UnHighLight()
     {
-        Outline outline = GetComponent<Outline>();
-        if (outline != null)
+        //Outline outline = GetComponent<Outline>();
+        //if (outline != null)
+        //{
+        //    Destroy(outline);
+        //}
+        //else
+        //{
+        //    Debug.Log("Outline component not found on edge object.");
+        //}
+
+        Renderer[] childrenRenderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in childrenRenderers)
         {
-            Destroy(outline);
-        }
-        else
-        {
-            Debug.Log("Outline component not found on edge object.");
+            rend.material = material;
         }
     }
 }
